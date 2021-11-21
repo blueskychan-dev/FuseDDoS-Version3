f01b32cd510335feae318e89240832613b4c5c8e
PC1266 <47820634+PC1266@users.noreply.github.com>
2021-11-21 17:45:08 +0700

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label6 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -46,11 +45,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(157, 9);
+            this.label1.Location = new System.Drawing.Point(159, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.Size = new System.Drawing.Size(107, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to DDoS Panel!";
+            this.label1.Text = "Welcome FuseDDoS";
             // 
             // label2
             // 
@@ -82,24 +81,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(146, 105);
+            this.button1.Location = new System.Drawing.Point(147, 105);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 25);
+            this.button1.Size = new System.Drawing.Size(145, 25);
             this.button1.TabIndex = 10;
-            this.button1.Text = "ATTACK with Bigspammer";
+            this.button1.Text = "ATTACK with BigSpammer";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(160, 254);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 11;
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label6
             // 
@@ -120,9 +108,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(185, 195);
+            this.button2.Location = new System.Drawing.Point(157, 167);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(109, 25);
             this.button2.TabIndex = 17;
             this.button2.Text = "STOP";
             this.button2.UseVisualStyleBackColor = true;
@@ -135,18 +123,18 @@
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(53, 13);
             this.label7.TabIndex = 18;
-            this.label7.Text = "Beta v0.5";
+            this.label7.Text = "Beta v0.7";
             this.label7.Click += new System.EventHandler(this.label7_Click_1);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(146, 136);
+            this.button3.Location = new System.Drawing.Point(147, 136);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(152, 25);
+            this.button3.Size = new System.Drawing.Size(145, 25);
             this.button3.TabIndex = 19;
-            this.button3.Text = "ATTACK with Big Flood";
+            this.button3.Text = "ATTACK with Flooding";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
             // Form1
             // 
@@ -158,14 +146,13 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "DDoS Panel";
+            this.Text = "FuseDDoS Version 3 (Beta v0.7)";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -179,7 +166,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox2;
